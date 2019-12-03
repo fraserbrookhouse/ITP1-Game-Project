3665686ed1bba9cee860bd78a572ffc8439a1d8a
Fraser Brookhouse <fbrookhouse@gmail.com>
2019-12-03 18:04:29 +0800

    completed 3b - added collectable logic, gravity, and jumping over canyon.

--- 3a:b/sketch.js
+++ 3a:b/sketch.js
@@ -12,10 +12,19 @@ Game interaction
 var gameChar_x;
 var gameChar_y;
 var floorPos_y;
+
 var isLeft;
 var isRight;
 var isFalling;
 var isPlummeting;
+
+var collectable;
+
+var canyon;
+var mountain;
+var cloud;
+var treePos_x;
+var treePos_y;
 
 
 function setup()
@@ -24,10 +33,19 @@ function setup()
 	floorPos_y = height * 3/4;
 	gameChar_x = width/2;
 	gameChar_y = floorPos_y;
+	
 	isLeft = false
 	isRight = false
 	isFalling = false
 	isPlummeting = false
+	
+	collectable = {x_pos: 50, y_pos: floorPos_y, size: 50, isFound: false}
+
+	canyon = {x_pos: 150, width: 150}
+	mountain = {x_pos: 0, size: 60}
+	cloud = {x_pos: 200, size: 60}
+	treePos_x = width/2;
+	treePos_y = floorPos_y - 150;
 }
 
 function draw()
@@ -42,7 +60,55 @@ function draw()
 	fill(0,155,0);
 	rect(0, floorPos_y, width, height - floorPos_y); //draw some green ground
 
-	//draw the canyon
+	//Draw cloud
+	fill(255, 255, 255);
+	ellipse(cloud.x_pos, 150, 80 * (cloud.size/50));
+	ellipse(cloud.x_pos - 40 * (cloud.size/50), 150, 60 * (cloud.size/50));
+	ellipse(cloud.x_pos + 40 * (cloud.size/50), 150, 60 * (cloud.size/50));
+
+	//Draw mountain
+	fill(150, 150, 150);
+	triangle(mountain.x_pos + 600 * (mountain.size/50), floorPos_y, mountain.x_pos  + 400 * (mountain.size/50), floorPos_y, mountain.x_pos + 500 * (mountain.size/50), floorPos_y - 332 * (mountain.size/50));
+	triangle(mountain.x_pos + 550 * (mountain.size/50), floorPos_y, mountain.x_pos + 650 * (mountain.size/50), floorPos_y, mountain.x_pos + 600 * (mountain.size/50), floorPos_y - 232 * (mountain.size/50));
+	triangle(mountain.x_pos + 350 * (mountain.size/50), floorPos_y, mountain.x_pos + 450 * (mountain.size/50), floorPos_y, mountain.x_pos + 400 * (mountain.size/50), floorPos_y - 232 * (mountain.size/50));
+	//snow cap
+	fill(255, 255, 255);
+	triangle(mountain.x_pos + 500 * (mountain.size/50), floorPos_y - 332 * (mountain.size/50), mountain.x_pos + 515 * (mountain.size/50), floorPos_y - 282 * (mountain.size/50), mountain.x_pos + 485 * (mountain.size/50), floorPos_y - 282 * (mountain.size/50));
+
+	//Draw tree
+	//Trunk
+	fill(120, 100, 40);
+	rect(treePos_x, treePos_y, 60, 150);
+	//Branches
+	fill(0, 155, 0);
+	triangle(treePos_x - 50, treePos_y + 50, treePos_x + 30, treePos_y - 50, treePos_x + 110, treePos_y + 50);
+	triangle(treePos_x - 50, treePos_y, treePos_x +30, treePos_y - 100, treePos_x + 110, treePos_y);
+
+	//Draw canyon
+	beginShape();
+	fill(139, 69, 19);
+	vertex(canyon.x_pos, floorPos_y); //150, 432
+	vertex(canyon.x_pos + 5, floorPos_y + 30);
+	vertex(canyon.x_pos + 15, floorPos_y + 60);
+	vertex(canyon.x_pos + 25, floorPos_y + 90);
+	vertex(canyon.x_pos + 35, floorPos_y + 144);
+	vertex(canyon.x_pos + 45 * (canyon.width/100), floorPos_y + 144);
+	vertex(canyon.x_pos + 55 * (canyon.width/100), floorPos_y + 90);
+	vertex(canyon.x_pos + 65 * (canyon.width/100), floorPos_y + 60);
+	vertex(canyon.x_pos + 75 * (canyon.width/100), floorPos_y + 30);
+	vertex(canyon.x_pos + 85 * (canyon.width/100), floorPos_y);
+	endShape();
+
+	//Draw collectable - coin
+	if(dist(gameChar_x, gameChar_y, collectable.x_pos, collectable.y_pos) < 15)
+	{
+		collectable.isFound = true;
+	}
+	if(collectable.isFound == false)
+	{
+		fill(255,215,0);
+		ellipse(collectable.x_pos, collectable.y_pos, collectable.size * 0.2);
+	}
 
 
 	//the game character
@@ -273,23 +339,30 @@ function draw()
 	//moves character left
 	if(isLeft == true)
 	{
-		gameChar_x -= 1;
+		gameChar_x -= 3.5;
 	}
 	//moves character right
 	if(isRight == true)
 	{
-		gameChar_x +=1;
+		gameChar_x +=3.5;
 	}
-
+	//gravity
 	if(gameChar_y < floorPos_y)
 	{
 		isFalling = true;
-		gameChar_y += 5 ;
+		gameChar_y += 5;
 	}
 	
 	else
 	{
 		isFalling = false;
+	}
+	// make character fall down canyon if with the canyon x co-ords and below the ground
+	if(gameChar_x > canyon.x_pos && gameChar_x < (canyon.x_pos + 85 * (canyon.width/100)) && gameChar_y >= floorPos_y - 5)
+	{
+		isPlummeting = true;
+		gameChar_y += 5;
+		gameChar_x = canyon.x_pos + (((canyon.x_pos + 85 * (canyon.width/100)) - canyon.x_pos) / 2); // ensures character falls down middle of canyon
 	}
 }
 
@@ -316,7 +389,7 @@ function keyPressed()
 	{
 		console.log("spacebarispressed")
 		if(gameChar_y == floorPos_y)
-		gameChar_y -= 100;
+		gameChar_y -= 200;
 	}
 }
 
